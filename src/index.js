d0c14b191afd10ad8f9e43f52f3dd20562e69220
Fith Wahome <wahomeimani@gmail.com>
2023-10-16 17:07:16 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,45 +1,52 @@
-javascript
+const cocktailSelect = document.getElementById("cocktailSelect");
+const getCocktailButton = document.getElementById("getCocktailButton");
+const cocktailDetails = document.getElementById("cocktailDetails");
+const cocktailName = document.getElementById("cocktailName");
+const cocktailImage = document.getElementById("cocktailImage");
+const cocktailInstructions = document.getElementById("cocktailInstructions");
+const cocktailIngredients = document.getElementById("cocktailIngredients");
 
-// Define the URL of the API you want to fetch data from
-const apiUrl = 'https://www.thecocktaildb.com/api.php';
-
-// Use the fetch function to make a GET request to the API
-fetch(apiUrl)
-  .then((response) => {
-    // Check if the response status is OK (status code 200)
-    if (response.status === 200) {
-      // Parse the response as JSON
-      return response.json();
-    } else {
-      // Handle the error (e.g., network error or non-200 status code)
-      throw new Error('Failed to fetch data');
-    }
-  })
-  .then((data) => {
-    // Use the fetched data
-    console.log(data);
-  })
-  .catch((error) => {
-    // Handle any errors that occurred during the fetch
-    console.error(error);
-  });
-
-
-
-
-
-
-
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelectorAll('nav ul li a').forEach(a => a.classList.remove('active'));
-        this.classList.add('active');
+// Fetch a list of cocktails and populate the select element
+fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
+    .then(response => response.json())
+    .then(data => {
+        data.drinks.forEach(cocktail => {
+            const option = document.createElement("option");
+            option.value = cocktail.idDrink;
+            option.textContent = cocktail.strDrink;
+            cocktailSelect.appendChild(option);
+        });
     });
-});
-javascript
-app.get('/search', (req, res) => {
- const searchQuery = req.query.query;
- // Implement your search algorithm here
- // For example, you can search for the query in your database
+
+// Event listener for the "Get Cocktail" button
+getCocktailButton.addEventListener("click", () => {
+    const selectedCocktailId = cocktailSelect.value;
+
+    if (selectedCocktailId) {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${selectedCocktailId}`)
+            .then(response => response.json())
+            .then(data => {
+                const drink = data.drinks[0];
+                cocktailName.textContent = drink.strDrink;
+                cocktailImage.src = drink.strDrinkThumb;
+                cocktailInstructions.textContent = drink.strInstructions;
+
+                // Clear previous ingredients
+                cocktailIngredients.innerHTML = "";
+
+                // Add ingredients and measures to the list
+                for (let i = 1; i <= 15; i++) {
+                    const ingredient = drink[`strIngredient${i}`];
+                    const measure = drink[`strMeasure${i}`];
+
+                    if (ingredient && measure) {
+                        const ingredientItem = document.createElement("li");
+                        ingredientItem.textContent = `${measure} of ${ingredient}`;
+                        cocktailIngredients.appendChild(ingredientItem);
+                    }
+                }
+
+                cocktailDetails.classList.remove("hidden");
+            });
+    }
 });
